e3e53d41338cd6b933cd97bef7f06f2110e64879
JianHu <swjtu_hj@163.com>
2022-09-06 00:36:01 +0100

    Use midthickness surface to project perfusion onto

--- project.py
+++ project.py
@@ -9,66 +9,82 @@ import nibabel as nib
 import os, ntpath
 from nilearn import datasets
 
-def project(fsdir, ref, output, src, surf, hemi, tf_mat, save_proj=True, load=True, projector=None):
+def project(fsdir, ref, output, src, tf_mat, save_proj=True, load=True, projector=None):
     logger = logging.getLogger("Toberone")
-    logger.info("Loading surfaces of reference image for {} {}.".format(surf, hemi))
-    '''
-    if hemi == 'left':
-        hs='lh'
-    elif hemi == 'right':
-        hs='rh'
-    else:
-        logger.info("Hemisphere does not exists.")
-    '''
+    logger.info("Loading surfaces of reference image for")
+
     LWS = os.path.join(fsdir, 'surf', 'lh.white')
     LPS = os.path.join(fsdir, 'surf', 'lh.pial')
     RWS = os.path.join(fsdir, 'surf', 'rh.white')
     RPS = os.path.join(fsdir, 'surf', 'rh.pial')
-    if hemi == 'left':
-        raw_hemi = tob.Hemisphere(LWS, LPS, side='L')
-    elif hemi == 'right':
-        raw_hemi = tob.Hemisphere(RWS, RPS, side='R')
-    else:
-        logger.info("Hemisphere does not exist.")
+
+    L_hemi = tob.Hemisphere(LWS, LPS, side='L')
+    R_hemi = tob.Hemisphere(RWS, RPS, side='R')
+    #
+    #logger.info("Hemisphere does not exist.")
 
     #projector = "{}_{}_projector.h5".format(surf, hemi)
     projector_path = projector
 
     t1_spc = rt.ImageSpace(ref)
 
-    if load == True and projector != None and os.path.exists(projector_path):
+    if load == True and os.path.exists(projector_path):
         logger.info("Load local projectors {}".format(projector_path))
         proj = tob.Projector.load(projector_path)
     else:
         logger.info("Ccomputing projectors")
-        proj = tob.Projector(raw_hemi, t1_spc)
+        proj = tob.Projector([L_hemi, R_hemi], t1_spc)
 
-    if  projector != None and os.path.exists(projector_path) and save_proj:
-        out_path = os.path.join(output, "{}_{}_projector.h5".format(surf, hemi))
-        proj.save(out_path)
-        logger.info("Projectors saved.")
+        if  save_proj:
+            out_path = os.path.join(output, "projector.h5")
+            proj.save(out_path)
+            logger.info("Projectors saved.")
     
     if tf_mat != None:
         logger.info("Transforming asl image from native space to structural space.")
-        asl2str_reg = rt.Registration.from_flirt(tf_mat, src=src, ref=ref)
-        asl_in_t1 = asl2str_reg.apply_to_image(src, ref=ref).get_fdata()
-
-        #asl_in_t1 = register_to_t1(src=src, ref=ref, tf_mat=tf_mat)
+        asl_in_t1 = register_to_t1(src=src, ref=ref, tf_mat=tf_mat)
     else:
         logger.info("ASL image is regarded in the structural space.")
         #To do: specify native space for ASL image
         asl_in_t1 = nib.load(src).get_fdata()
 
     prefix = ntpath.basename(src).split('.')[0]
-    print(proj)
+
     vol_on_surf = proj.vol2surf(vdata=asl_in_t1.flatten(), edge_scale=False)
 
 
+
     logger.info("Use fsaverage mesh to display metric files.")
-    fsaverage = datasets.fetch_surf_fsaverage()
-    surf = tob.Surface(fsaverage["{}_{}".format(surf, hemi)])
-    surf.save_metric(vol_on_surf, os.path.join(output, prefix + "_surf_on_{}_{}.func.gii".format(surf, hemi)))
-    logger.info("Metric file({} {}) on fsaverage saved.".format(surf, hemi))
+    #print(vol_on_surf.shape)
+    #l_surf = tob.Surface(LWS)
+    #print((l_surf.points))
+    #print(len(l_surf.points))
+
+    vol_on_surf = proj.vol2surf(vdata=asl_in_t1.flatten(), edge_scale=False)
+
+    for side, hemi in proj.hemi_dict.items():
+
+        midsurf = hemi.midsurface()
+        print(len(midsurf.points))
+        if side == 'L':
+            midsurf.save_metric(vol_on_surf[:len(midsurf.points)], os.path.join(output, prefix + "midsurf_{}.func.gii".format(side)))
+        else:
+            midsurf.save_metric(vol_on_surf[-len(midsurf.points):],
+                                os.path.join(output, prefix + "midsurf_{}.func.gii".format(side)))
+
+        #print()
+        print(side)
+        print(midsurf)
+
+
+    #l_midsurf = get_midthickness(L_hemi)[:len(L_hemi)/2]
+    #r_midsurf = get_midthickness(R_hemi)[len(L_hemi)/2:]
+    #print(type(l_midsurf))
+
+    #r_surf = tob.Surface(r_midsurf)
+    #l_midsurf.save_metric(vol_on_surf, os.path.join(output, prefix + "_surf_on_{}_{}.func.gii".format('mid', 'left')))
+    #r_midsurf.save_metric(vol_on_surf, os.path.join(output, prefix + "_surf_on_{}_{}.func.gii".format('mid', 'right')))
+    #logger.info("Metric file({} {}) on fsaverage saved.".format(surf, hemi))
 
 
     #surf_in_vol = proj.surf2vol(vol_on_surf, edge_scale=False)
@@ -78,6 +94,9 @@ def register_to_t1(src, ref, tf_mat):
     asl2str_reg = rt.Registration.from_flirt(tf_mat, src=src, ref=ref)
     asl_in_t1 = asl2str_reg.apply_to_image(src, ref=ref).get_fdata()
     return asl_in_t1
+
+def get_midthickness(hemi):
+    return hemi.midsurface()
 
 def main(): 
     parser = argparse.ArgumentParser(
@@ -109,18 +128,6 @@ def main():
     )
 
     parser.add_argument(
-        "--hemi",
-        help="The hemisphere which the projection based on",
-        required=False
-    )
-
-    parser.add_argument(
-        "--surf",
-        help="Projection for which surface(pial/white)",
-        required=True
-    )
-
-    parser.add_argument(
         "--asl2str",
         help="Path to the transformation matrix from native space to structural space",
         required=False
@@ -148,12 +155,10 @@ def main():
     output = args.output
     Path(output).mkdir(exist_ok=True)
     prefix = ntpath.basename(src).split('.')[0]
-    surf = args.surf
-    hemi = args.hemi
     logger = setup_logger("Toberone", os.path.join(output, prefix+'_tob_proj.log'), "INFO", args.verbose)
     logger.info(args)
 
-    project(fsdir=fs_dir, ref=ref, output=output, src=src, tf_mat=asl2str, save_proj=True, load=True, surf=surf, hemi=hemi, projector = projector)
+    project(fsdir=fs_dir, ref=ref, output=output, src=src, tf_mat=asl2str, save_proj=True, load=True, projector = projector)
 
     pass
 
